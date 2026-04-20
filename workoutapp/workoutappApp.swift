af1803adac284a410631a08eaf08b7f7b5f38396
Tan <tanveerbaidwan@gmail.com>
2026-04-20 11:05:45 +0100

    Initial Commit

---
+++ workoutapp/workoutappApp.swift
@@ -0,0 +1,17 @@
+//
+//  workoutappApp.swift
+//  workoutapp
+//
+//  Created by Tanveer Baidwan on 20/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct workoutappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
